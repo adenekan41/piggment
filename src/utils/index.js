cf6f75474fa3852b72f20dd83500d3e77ea074d0
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2020-06-12 23:01:09 +0100

    (ft): fixed contrast issues

--- src/utils/index.js
+++ src/utils/index.js
@@ -87,7 +87,11 @@ export const guidGenerator = () => {
  * @param {String} b
  */
 export const luminanace = (r, g, b) => {
-	return 0.2126 * r + 0.7152 * g + 0.0722 * b;
+	const a = [r, g, b].map(function(v) {
+		v /= 255;
+		return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
+	});
+	return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
 };
 
 /**
